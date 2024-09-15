501e838d860cd2d87b52c2c416f63b6b8202bbda
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-09-15 08:45:33 +0200

    feature: add route to get all products

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ import express from "express";
 const app = express()
 const PORT = 3000
 
+// dummy data:
+const products = [
+    { id: 1, name: "Apple", description: "Fruits from the hills of Rwanda", price: 350 },
+    { id: 2, name: "Chairs", description: "Fruits from the hills of Rwanda", price: 350 },
+    { id: 3, name: "Mona Lisa Painting", description: "Fruits from the hills of Rwanda", price: 350 },
+    { id: 4, name: "Bulova Watch", description: "Fruits from the hills of Rwanda", price: 350 },
+]
+
 app.use(express.json())
+
+app.get('/items', (req, res) => {
+    res.status(200).json(products)
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
