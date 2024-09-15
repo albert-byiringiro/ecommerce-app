cd88f6ae347d5c7958a263bf5617f17dc43fbef5
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-09-15 08:54:00 +0200

    feature: add route to get one product

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.get('/items', (req, res) => {
     res.status(200).json(products)
 })
 
+app.get('/items/:id', (req, res) => {
+    const product = products.find(product => product.id === parseInt(req.params.id));
+    if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+    res.status(200).json(product)
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
 })
